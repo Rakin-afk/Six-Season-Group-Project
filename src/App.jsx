368bc72e1913cd2e7934545c19e9c9dcb5f72a9e
Rakin <azmainesadik@gmail.com>
2026-06-18 17:09:30 +0600

    add scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop' // 👈 ১. প্রথমে ইমপোর্ট করুন
 import Home from './pages/Home'
 import AboutUs from './pages/AboutUs' 
 import Contact from './pages/Contact'
 import Management from './pages/Management'
-// 🆕 শুধুমাত্র ১ম পেজটি ইমপোর্ট থাকবে
 import BoardDirectors1 from './pages/BoardDirectors1'
 import TeamLeaders from './pages/TeamLeaders'
 
 function App() {
   return (
-    // 🎯 নিচে basename যুক্ত করা হয়েছে যাতে GitHub Pages-এ ব্ল্যাঙ্ক স্ক্রিন না আসে
     <BrowserRouter basename="/Six-Season-Group-Project">
+      {/* 🎯 ২. এখানে ScrollToTop বসিয়ে দিন, তাহলে সব পেজের জন্য একবারে কাজ করবে */}
+      <ScrollToTop /> 
+
       <div className="flex flex-col min-h-screen bg-[#E7FBF3]">
         <Navbar />
         
@@ -24,8 +26,6 @@ function App() {
             <Route path="/about" element={<AboutUs />} /> 
             <Route path="/contact" element={<Contact />} />
             <Route path="/management" element={<Management />} /> 
-            
-            {/* 🎯 বাটনে ক্লিক করলে এই পাথে চলে আসবে */}
             <Route path="/board-of-directors" element={<BoardDirectors1 />} />
             <Route path="/team-leaders" element={<TeamLeaders />} />
           </Routes>
